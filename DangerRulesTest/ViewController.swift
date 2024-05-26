321bb3f41df484df0b4c396a7cab59bc5152357e
Mayur Gauswami <mayurgiri.gauswami@go-mmt.com>
2024-05-26 08:57:01 +0530

    Initial changes

---
+++ DangerRulesTest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DangerRulesTest
+//
+//  Created by Mayur Gauswami on 26/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var testVar:String = "abc"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
